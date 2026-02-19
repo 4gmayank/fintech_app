e8143da543b2d664f1fdfe35807d1acf3d6a75d6
Mayank Goyal <mayankgoyal.mait@gmail.com>
2026-02-19 19:09:13 +0530

    fix-ui

--- lib/domain/item_entity.dart
+++ lib/domain/item_entity.dart
@@ -2,7 +2,7 @@ import 'package:fintech_app/data/product_response_model.dart';
 
 class ItemEntity {
   String title;
-  String imageUrl;
+    String imageUrl;
   String description;
 
   ItemEntity(this.title, this.imageUrl, this.description);
--- lib/presentation/item_list_screen.dart
+++ lib/presentation/item_list_screen.dart
@@ -16,15 +16,12 @@ class _ItemListScreenState extends State<ItemListScreen> {
     return Scaffold(
       appBar: AppBar(title: Text("Product List")),
       body: BlocConsumer<ProductBloc, ProductState>(
-          listener: (context, state) {
-
-          },
+          listener: (context, state) {},
           builder: (context, state) {
             if (state is ProductSuccess) {
-
               return productList(state.itemList);
             }
-            if(state is ProductInitial){
+            if (state is ProductInitial) {
               context.read<ProductBloc>().add(GetProduct());
             }
             return const Center(
@@ -43,16 +40,61 @@ class _ItemListScreenState extends State<ItemListScreen> {
   }
 
   Widget component(ItemEntity item) {
-    return Row(
-      children: [
-        // Image(image: ImageProvider),//item.fetchImageUrl()
-        Column(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(item.title),
-            Text(item.description),
+            Container(
+              height: 80,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 80, maxHeight: 80),
+                child: Image.network(
+                  item.imageUrl,
+                  loadingBuilder: (BuildContext context, Widget child,
+                      ImageChunkEvent? loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes!
+                            : null,
+                      ),
+                    );
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Icon(
+                        Icons.error); // Widget to display on error
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.title,
+                    style: TextStyle(fontSize: 14),
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    item.description,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(fontSize: 10),
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
